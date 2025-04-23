3ec4dcb0ad5047201c7303e242c7e684a18558bb
yoruwitch <evelyn.fernandes4@gmail.com>
2025-04-23 15:36:00 -0300

    feat: refactor App component to include logo and navigation options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./components/Logo";
+import profile from "./images/perfil.svg";
+import shopbag from "./images/sacola.svg";
+
+const textOptions = ["Categories", "My booklist", "Favorites"];
+const icons = [profile, shopbag];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          teste
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Logo />
+                <ul className="options">
+                    {textOptions.map((text) => (
+                        <li className="option">
+                            <p>{text}</p>
+                        </li>
+                    ))}
+                </ul>
+                <ul className="icons">
+                    {icons.map((icon) => (
+                        <li>
+                            <img
+                                alt="Shop icon"
+                                className="icon"
+                                src={icon}
+                            ></img>
+                        </li>
+                    ))}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
